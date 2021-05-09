d5cdd3a95c3a49b1064d70cddb74f8308efbd376
Gary Larson <gary.larson316@gmail.com>
2021-05-09 01:29:17 -0700

    CircularLinkedList Complete: 100% Line Coverage

--- src/CircularLinkedList.java
+++ src/CircularLinkedList.java
@@ -1,10 +1,24 @@
 import java.util.Iterator;
 
+/**
+ * represents a circular linked list
+ * @author Gary Larson
+ * @version 5/9/2021
+ */
 public class CircularLinkedList<E> implements Iterable<E>  {
+    /** reference pointer to the beginning of the list */
     private Node<E> front;
+    /** lazy pointer used in remove */
+    private Node<E> prev;
+    /** reference pointer to the end of the list */
     private Node<E> end;
+    /** number of elements in the list */
     private int size;
 
+
+    /**
+     * constructs CircularLinkedList
+     */
     public CircularLinkedList() {
         front = null;
         end = null;
@@ -12,68 +26,184 @@ public class CircularLinkedList<E> implements Iterable<E>  {
     }
 
 
+    /**
+     * add an element to the list
+     * @param data input
+     */
     public void add(E data) {
         if (data == null) {
+            //todo: exception test
             throw new IllegalArgumentException("data cannot be null");
         }
 
         Node<E> newNode = new Node<>(data);
         //logic to ensure end is always updated
         if (end == null) {
+            front = newNode;
+            end = newNode;
+            front.next = end;
+        }
+        else {
+            end.next = newNode;
             end = newNode;
         }
-        newNode.next = front;
-        front = newNode;
+        end.next = front;
         size++;
 
     }
 
 
-    public void addAtEnd(E data) {
-        //todo: preconditions data cannot be null
-        Node<E> newNode = new Node<>(data);
-        if (front == null) {
-           front = newNode;
-        } else {
-            Node<E> current = front;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
+    /**
+     * return an element from given position
+     * @param pos desired position
+     * @return element from given position
+     */
+    public E get(int pos) {
+        checkBounds(pos);
+        Node<E> curr = front;
+        for (int i = 1; i < pos; i++) {
+            curr = curr.next;
         }
-        size++;
+        return curr.data;
     }
 
 
-    public void remove(E value) {
-
-    }
-
-
+    /**
+     * remove an element at the given position
+     * @param pos position of the elmement to remove
+     */
     public void remove(int pos) {
+        checkBounds(pos);
+        Node<E> curr = front;
+        Node<E> prev = null;
 
+        for (int i = 1; i < pos; i++) {
+            prev = curr;
+            curr = curr.next;
+        }
+
+        if (pos == 1) {
+            front = curr.next;
+            end.next = front;
+        }
+        else if (pos == size) {
+           curr.next = front;
+           end = curr;
+        }
+        else {
+            prev.next = curr.next;
+        }
+        size--;
     }
 
 
+    /**
+     * removes the first occurrence of the given value
+     * @param value given value
+     * @return true if successful
+     */
+    public boolean remove(E value) {
+        boolean containsValue = true;
+        Node<E> curr = front;
+        int counter = 0;
+
+        //empty list
+        if (size == 0) {
+            return false;
+        }
+        //only one item
+        if (size == 1 && front.data == value ) {
+            front = null;
+            end = null;
+            size--;
+            return true;
+        }
+
+        //traverse for value
+        while (curr.next.data != value && containsValue) {
+            prev = curr;
+            curr = curr.next;
+            counter++;
+            if (counter > size) {
+                containsValue = false;
+            }
+        }
+
+        if (containsValue) {
+            //is it the front?
+            if (curr.next == front) {
+                front = curr.next.next;
+                end.next = front;
+            }
+            //is it the end?
+            else if (curr.next == end) {
+                curr.next = front;
+                end = curr;
+
+            }
+            //is it the middle?
+            else {
+                curr.next = curr.next.next;
+            }
+            size--;
+        }
+
+        return containsValue;
+    }
+
+    /**
+     * clears the list of all elements
+     */
     public void clear() {
         front = null;
+        size = 0;
     }
 
-
+    /**
+     * size of the list
+     * @return the size of the list
+     */
     public int size() {
         return size;
     }
 
 
+    /**
+     * helper function to check bounds of position input
+     * @param pos desired position
+     */
+    private void checkBounds(int pos) {
+        if( pos < 1 || pos > size()) {
+            //todo: exception test
+            String msg = String.format("The index must be between 1 and the size: %d, inclusive.", size());
+            if (size < 1) {
+                msg = "Can't access items in a size 0 list.";
+            }
+            throw new IndexOutOfBoundsException(msg);
+        }
+    }
+
+
+    /**
+     * circular iterator; moves from end to front continuously
+     * @return iterator
+     */
     public Iterator<E> iterator() {
         return new CircularLinkedListIterator();
     }
 
-
+    /**
+     * represents a node to store data
+     * @param <T> given Type
+     */
     private static class Node<T> {
         public T data;
         public Node<T> next;
 
+        /**
+         * constructs a Node
+         * @param data value the node will contain
+         */
         public Node(T data) {
             this.data = data;
             next = null;
@@ -82,51 +212,51 @@ public class CircularLinkedList<E> implements Iterable<E>  {
     }
 
 
+    /**
+     * represents an Iterator for CircularLinkedList
+     */
     public class CircularLinkedListIterator implements Iterator<E> {
         Node<E> curr;
         Node<E> prev;
         boolean removeOk;
 
+
+        /**
+         * constructs the iterator
+         */
         public CircularLinkedListIterator() {
             removeOk = false;
             curr = front;
         }
 
+
+        /**
+         *  verifies there is another element
+         * @return true if theres another element in the list
+         */
         public boolean hasNext() {
-            if(curr == null) {
-                curr = front;
-            }
-
-            return curr != null || size > 1;
-
+           return size > 1;
         }
 
-
+        /**
+         * moves to the next element
+         * @return data in current element
+         */
         public E next() {
             E result = null;
-            result = curr.data;
             if (hasNext()) {
+                result = curr.data;
                 prev = curr;
-                curr = curr.next;
+                if (curr == end) {
+                    curr = front;
+                }
+                else {
+                    curr = curr.next;
+                }
                 removeOk = true;
             }
             return result;
         }
-
-        public void remove() {
-            //Removes from the underlying collection the last element returned by this iterator
-            //removes what previous is pointing at
-            if (removeOk) {
-                if (curr == null) {
-                    prev.next = null;
-                } else {
-                    prev.next = curr.next;
-                }
-
-                size--;
-            }
-        }
-
     }
 
 }
--- src/CircularLinkedListTest.java
+++ src/CircularLinkedListTest.java
@@ -1,10 +1,13 @@
 import org.junit.jupiter.api.Test;
-
 import java.util.Iterator;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CircularLinkedListTest {
+
+    ////////////////////////////////////////////////////////////////////
+    //                    HAPPY PATH TESTS
+    ////////////////////////////////////////////////////////////////////
 
     @Test
     void add() {
@@ -17,25 +20,137 @@ class CircularLinkedListTest {
         assertEquals(5,myInts.size());
     }
 
+
     @Test
-    void addAtEnd() {
+    void get() {
+        CircularLinkedList<String> myInts = new CircularLinkedList<>();
+        myInts.add("a");
+        myInts.add("b");
+        myInts.add("c");
+        assertEquals("a",myInts.get(1));
+        assertEquals("b",myInts.get(2));
+        assertEquals("c",myInts.get(3));
+    }
+
+
+    @Test
+    void removeByPosFirst() {
+        CircularLinkedList<String> myInts = new CircularLinkedList<>();
+        myInts.add("a");
+        myInts.add("b");
+        myInts.add("c");
+        myInts.remove(1);
+        assertEquals("b", myInts.get(1));
+        myInts.remove(1);
+        assertEquals("c", myInts.get(1));
+        myInts.remove(1);
+        assertEquals(0, myInts.size());
+    }
+
+
+    @Test
+    void removeByPosLast() {
+        CircularLinkedList<String> myInts = new CircularLinkedList<>();
+        myInts.add("a");
+        myInts.add("b");
+        myInts.add("c");
+        myInts.remove(myInts.size());
+        assertEquals("b", myInts.get(myInts.size()));
+        myInts.remove(myInts.size());
+        assertEquals("a", myInts.get(myInts.size()));
+        myInts.remove(myInts.size());
+        assertEquals(0, myInts.size());
+
+
+    }
+    @Test
+    void removeByPosMiddle() {
+        CircularLinkedList<String> myInts = new CircularLinkedList<>();
+        myInts.add("a");
+        myInts.add("b");
+        myInts.add("c");
+        myInts.remove(2);
+        assertEquals("c", myInts.get(myInts.size()));
+        myInts.add("d");
+        myInts.remove(2);
+        assertEquals("d", myInts.get(myInts.size()));
+
+
+
+
     }
 
     @Test
-    void remove() {
+    void removeFromNullByValue() {
+        CircularLinkedList<String> myInts = new CircularLinkedList<>();
+        assertEquals(false,myInts.remove("1"));
+    }
+
+
+    @Test
+    void removeByValueSizeOne() {
+        CircularLinkedList<String> myInts = new CircularLinkedList<>();
+        myInts.add("1");
+        myInts.remove("1");
+        assertTrue(myInts.size() == 0);
+    }
+
+
+    @Test
+    void removeByValueNotPresent() {
+        CircularLinkedList<String> myInts = new CircularLinkedList<>();
+        myInts.add("1");
+        myInts.add("2");
+        myInts.add("3");
+        assertEquals(false,myInts.remove("4"));
     }
 
     @Test
-    void testRemove() {
+    void removeFirstValue() {
+        CircularLinkedList<String> myInts = new CircularLinkedList<>();
+        myInts.add("1");
+        myInts.add("2");
+        myInts.add("3");
+        myInts.remove("1");
+        assertEquals("2", myInts.get(1));
+        myInts.remove("2");
+        assertEquals("3", myInts.get(1));
     }
 
-    @Test
-    void clear() {
-    }
 
     @Test
-    void size() {
+    void removeMiddleValue() {
+        CircularLinkedList<String> myInts = new CircularLinkedList<>();
+        myInts.add("1");
+        myInts.add("2");
+        myInts.add("3");
+        myInts.remove("2");
+        assertEquals("3", myInts.get(2));
     }
+
+
+    @Test
+    void removeLastValue() {
+        CircularLinkedList<String> myInts = new CircularLinkedList<>();
+        myInts.add("1");
+        myInts.add("2");
+        myInts.add("3");
+        myInts.remove("3");
+        assertEquals(2, myInts.size());
+    }
+
+
+    @Test
+    void clearAndSize() {
+        CircularLinkedList<String> myInts = new CircularLinkedList<>();
+        myInts.add("1");
+        myInts.add("2");
+        myInts.add("3");
+        myInts.clear();
+        assertTrue(myInts.size() == 0);
+
+    }
+
 
     @Test
     void iterator() {
@@ -43,13 +158,37 @@ class CircularLinkedListTest {
         myInts.add(5);
         myInts.add(4);
         myInts.add(3);
-        myInts.add(2);
-        myInts.add(1);
         Iterator<Integer> iter = myInts.iterator();
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(iter.next());
-            iter.remove();
-        }
+        assertTrue(iter.hasNext());
+        assertEquals(5,iter.next());
+        assertEquals(4,iter.next());
+        assertEquals(3,iter.next());
+        assertEquals(5,iter.next());
+        assertEquals(4,iter.next());
+        assertEquals(3,iter.next());
+    }
+
+
+    ////////////////////////////////////////////////////////////////////
+    //                    EXCEPTION TESTS
+    ////////////////////////////////////////////////////////////////////
+
+
+    @Test
+    void removeFromNullByPos() {
+        CircularLinkedList<String> myInts = new CircularLinkedList<>();
+        assertThrows(IndexOutOfBoundsException.class,() -> {
+            myInts.remove(1);
+        });
+    }
+
+    @Test
+    void inputNullData() {
+        CircularLinkedList<String> myInts = new CircularLinkedList<>();
+        assertThrows(IllegalArgumentException.class, () -> {
+            myInts.add(null);
+        });
+
     }
 }
